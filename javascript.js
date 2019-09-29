323cc3d3c669771affc443adc35a965676acda0c
thanhle <mix2pigcat@gmaail.com>
2019-09-29 11:53:37 +0700

    filebai

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function toantu(){
+    let message,login;
+    login=document.getElementById('login').value;
+    if(login=="employee"){
+    alert("hello")
+    }
+    else if(login=="director"){
+        alert("gretting");
+    }
+    else if(login==""){
+        alert("no login");
+    }
+    else{
+        message="";
+        alert("");
+    }
+}
+
